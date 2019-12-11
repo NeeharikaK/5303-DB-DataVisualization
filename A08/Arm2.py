c878752fac544c440eaf69908eede30d84094a8f
NeeharikaK <32913054+NeeharikaK@users.noreply.github.com>
2019-12-11 10:14:48 -0600

    Top 3 worst PEI's

---
+++ A08/Arm2.py
@@ -0,0 +1,48 @@
+
+import numpy as np 
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+import pymongo
+
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["armageddon"]
+token = "pk.eyJ1IjoibmVlaGFyaWthayIsImEiOiJjazJsOTlsYWcwNHFtM2JudHRpY2Y1ZnUzIn0.-9h08W2-BvmvhGfTO8Mobg"
+volc = db["volcanos"]
+mapbox_style = "mapbox://styles/neeharikak/ck3yt0g624vwr1clm0itq8t5r"
+
+listvolcanos = []
+lats = []
+lons = []
+for obj in volc.find():
+    listvolcanos.append(db["volcanos"].find().sort([("PEI", -1)]))
+    lats.append(obj["latitude"])
+    lons.append(obj["longitude"])
+fig = go.Figure()
+
+fig.add_trace(go.Scattermapbox(
+                                    lat = lats,
+                                    lon = lons,
+                                    mode = "markers",   
+                                    marker = {"size":[30, 30, 30],
+                                    "color":["Red", "Orange", "Yellow"]}
+                                    )
+                                )
+
+fig.update_layout(
+        autosize=True,
+        hovermode='closest',
+        mapbox=go.layout.Mapbox(
+            accesstoken=token,
+            bearing=0,
+            style=mapbox_style,
+            center=go.layout.mapbox.Center(
+                lat=33.930828,
+                lon=-98.484879
+            ),
+            pitch=0,
+            zoom=3
+        ))
+
+fig.show()
+plotly.offline.plot(fig, filename='listvolcanos.html')
